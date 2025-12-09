548644b0342dd877dd76ab371b44a1b944a0c98f
matwey <shafraneko.exe@gmail.com>
2025-12-09 21:11:46 +0200

    Лабораторна 6

---
+++ LibraryManagementSystem/LibraryManagementSystem/LibraryCatalog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+    public class LibraryCatalog<T> where T : ILibraryItem
+    {
+        private List<T> _items = new List<T>();
+
+        public void AddItem(T item)
+        {
+            _items.Add(item);
+        }
+
+        public List<T> GetAllItems()
+        {
+            return new List<T>(_items);
+        }
+        public T GetItemById(int id) 
+        {
+            foreach (var item in _items)
+            {
+                if(item.Id == id)
+                {
+                    return item;
+                }
+            }
+            return default(T);
+        }
+    }
+}
---
+++ LibraryManagementSystem/LibraryManagementSystem/LibraryManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+    public class LibraryManager
+    {
+        private LibraryCatalog<Book> _bookCatalog;
+
+        private LibraryCatalog<Magazine> _magazineCatalog;
+
+        public LibraryManager()
+        {
+            _bookCatalog = new LibraryCatalog<Book>();
+            _magazineCatalog = new LibraryCatalog<Magazine>();
+        }
+
+        public void AddItem(ILibraryItem item)
+        {
+            if(item is Book b)
+            {
+                _bookCatalog.AddItem(b);
+            }
+            else if (item is Magazine m)
+            {
+                _magazineCatalog.AddItem(m);
+            }
+        }
+        public List<ILibraryItem> GetAllItems()
+        {
+            List<ILibraryItem> all = new List<ILibraryItem>();
+            all.AddRange( _bookCatalog.GetAllItems());
+            all.AddRange(_magazineCatalog.GetAllItems());
+            return all;
+        }
+
+        public ILibraryItem? GetItemById(int id)
+        {
+            var book = _bookCatalog.GetItemById(id);
+            if (book != null)
+            {
+                return book;
+            }
+
+            var magazine = _magazineCatalog.GetItemById(id);
+            if(magazine != null)
+            {
+                return magazine;
+            }
+
+            return null;
+        }
+    }
+}
